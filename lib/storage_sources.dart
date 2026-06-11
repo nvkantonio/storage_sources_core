1350d1260dfc6396dffb3f03505c3e52ae3f6c4e
nvkantonio <nvkantonio@gmail.com>
2026-06-11 15:00:57 +0200

    refactor: file organization

--- lib/storage_sources.dart
+++ lib/storage_sources.dart
@@ -2,14 +2,14 @@ library;
 
 export 'src/storage/storage.dart';
 export 'src/storage_source/storage_source.dart';
-export 'src/storage_sources.dart';
+export 'src/storage_sources/storage_sources.dart';
 
 export 'src/storage_source/storage_source_proxy.dart';
 
 export 'src/utils/storage_source_result.dart';
 
-export 'src/complete_storage_sources/callback_storage_source.dart';
-export 'src/complete_storage_sources/network_source.dart';
+export 'src/complete_storage_source/callback_storage_source.dart';
+export 'src/complete_storage_source/network_source.dart';
 export 'src/storage_source/database_storage_source.dart';
 
 export 'src/complete_storage/cache_or_headmost_storage.dart';
--- lib/storage_sources_core.dart
+++ lib/storage_sources_core.dart
@@ -2,7 +2,7 @@ library;
 
 export 'src/storage/storage.dart';
 export 'src/storage_source/storage_source.dart';
-export 'src/storage_sources.dart';
+export 'src/storage_sources/storage_sources.dart';
 
 export 'src/storage_source/storage_source_proxy.dart';
 
