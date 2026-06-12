66b85eed44287daa5276d5ad9a25e8488e3b78c9
nvkantonio <nvkantonio@gmail.com>
2026-06-13 01:05:50 +0200

    add feat: CallbackCompleter equality args Equality args return result from previous call of completer if equal. If unequal waits for previous result completion and runs new callback

--- lib/src/utils/callback_completer.dart
+++ lib/src/utils/callback_completer.dart
@@ -1,21 +1,64 @@
 import 'dart:async';
 
+final class NoArgument {
+  const NoArgument();
+
+  @override
+  int get hashCode => super.hashCode;
+
+  @override
+  bool operator ==(Object other) => other is NoArgument;
+}
+
+final class _CompleterWithArgument<T> {
+  _CompleterWithArgument(this.equalityArg) : completer = Completer<T>();
+
+  final Completer<T> completer;
+  final dynamic equalityArg;
+}
+
 class CallbackCompleter<T> {
   CallbackCompleter();
 
-  Completer<T>? _completer;
+  _CompleterWithArgument<T>? _completerWithArg;
+
+  Completer<T>? get _completer => _completerWithArg?.completer;
 
   bool get isInProgress => _completer != null && !_completer!.isCompleted;
 
   Future<T>? get future => _completer?.future;
 
-  Future<T> run(Future<T> Function() callback) async {
-    if (_completer != null) {
-      return _completer!.future;
+  Future<T> run(Future<T> Function() callback,
+      [dynamic equalityArg = const NoArgument()]) {
+    final currentCompleterWithArg = _completerWithArg;
+
+    if (currentCompleterWithArg == null) {
+      final newCompleterWithArg = _CompleterWithArgument<T>(equalityArg);
+      _completerWithArg = newCompleterWithArg;
+
+      return _runCompleter(callback, newCompleterWithArg);
     }
 
-    final completer = Completer<T>();
-    _completer = completer;
+    if (equalityArg == currentCompleterWithArg.equalityArg) {
+      return currentCompleterWithArg.completer.future;
+    }
+
+    final newCompleterWithArg = _CompleterWithArgument<T>(equalityArg);
+    _completerWithArg = newCompleterWithArg;
+
+    final currentCompleterFuture =
+        Future<dynamic>(() => currentCompleterWithArg.completer.future);
+
+    return currentCompleterFuture.catchError((_) => null).then(
+      (_) {
+        return _runCompleter(callback, newCompleterWithArg);
+      },
+    );
+  }
+
+  Future<T> _runCompleter(Future<T> Function() callback,
+      _CompleterWithArgument<T> completerWithArg) {
+    final completer = completerWithArg.completer;
 
     Future(
       () async {
@@ -26,12 +69,12 @@ class CallbackCompleter<T> {
           completer.completeError(e, st);
         }
       },
-    )..whenComplete(() {
-        return _completer = null;
-      });
+    ).whenComplete(() {
+      if (identical(completerWithArg, _completerWithArg)) {
+        _completerWithArg = null;
+      }
+    });
 
     return completer.future;
-
-    // TODO
   }
 }
--- test/callback_completer_test.dart
+++ test/callback_completer_test.dart
@@ -7,89 +7,214 @@ typedef SrUndef<T> = UndefinedStorageSourceResult<T>;
 typedef SrError<T> = ErrorStorageSourceResult<T>;
 
 void main() {
-  group('A group of CallbackCompleter test', () {
-    const testString = 'yes-yes-yes';
-    const testString2 = '2';
-    const testString3 = '3';
-    const testString4 = '4';
-    const testStringError = 'error';
+  const testString1 = '1';
+  const testString2 = '2';
+  const testString3 = '3';
+  const testString4 = '4';
 
-    Future<String> futureTestString(String testString) =>
-        Future<String>.delayed(
-          Duration(milliseconds: 50),
-          () => testString,
-        );
+  const testStringError = 'error';
 
-    Future<String> futureCauseError(String exceptionMessage) async {
-      return Future<String>.delayed(
+  Future<String> futureTestString(String testString) => Future<String>.delayed(
         Duration(milliseconds: 50),
-        () => throw '$testStringError:$exceptionMessage',
+        () => testString,
       );
-    }
 
-    test('Test CallbackCompleter runner', () async {
+  Future<String> futureCauseError(String exceptionMessage) async {
+    return Future<String>.delayed(
+      Duration(milliseconds: 50),
+      () => throw '$testStringError:$exceptionMessage',
+    );
+  }
+
+  group('A group of CallbackCompleter tests without args (same as equal args)',
+      () {
+    test('Test CallbackCompleter runner. No args', () async {
       final callbackCompleter = CallbackCompleter<String>();
 
       expect(
-          await callbackCompleter.run(() async => futureTestString(testString)),
-          testString);
+          await callbackCompleter
+              .run(() async => futureTestString(testString1)),
+          testString1);
 
-      expect(await callbackCompleter.run(() => futureTestString(testString)),
-          testString);
+      expect(await callbackCompleter.run(() => futureTestString(testString2)),
+          testString2);
     });
 
-    test('Test CallbackCompleter multiple call must result in save response',
-        () async {
+    test('Test CallbackCompleter multiple call. No args', () async {
       final callbackCompleter = CallbackCompleter<String>();
 
       final res1 = Future(
-          () => callbackCompleter.run(() => futureTestString(testString)));
+          () => callbackCompleter.run(() => futureTestString(testString1)));
       final res2 = Future(
           () => callbackCompleter.run(() => futureTestString(testString2)));
       final res3 = Future(
           () => callbackCompleter.run(() => futureTestString(testString3)));
 
-      expect(await res1, testString);
+      expect(await res1, testString1);
 
       final res4 = Future(
           () => callbackCompleter.run(() => futureTestString(testString4)));
 
-      expect(await res2, testString);
-      expect(await res3, testString);
+      expect(await res2, testString1);
+      expect(await res3, testString1);
 
       expect(await res4, testString4);
     });
 
-    test('Test CallbackCompleter exceptions handle. Must not throw', () async {
+    test('Test CallbackCompleter exceptions handle. No args', () async {
       final callbackCompleter = CallbackCompleter<String>();
 
       final res1 = Future(
-              () => callbackCompleter.run(() => futureTestString(testString)))
-          .catchError((e, st) => '$e:$testString');
+              () => callbackCompleter.run(() => futureTestString(testString1)))
+          .catchError((e, st) => '$e:$testString1');
 
       final res2 = Future(
               () => callbackCompleter.run(() => futureTestString(testString2)))
           .catchError((e, st) => '$e:$testString2');
 
-      expect(await res1, testString);
-      expect(await res2, testString);
+      expect(await res1, testString1);
+      expect(await res2, testString1);
     });
 
     test(
-        'Test CallbackCompleter exceptions handle. Must throw and .catchError must replace exception',
+        'Test CallbackCompleter exceptions handle. No args. Must throw and .catchError must replace exception',
         () async {
       final callbackCompleter = CallbackCompleter<String>();
 
       final res1 = Future(
-              () => callbackCompleter.run(() => futureCauseError(testString)))
+              () => callbackCompleter.run(() => futureCauseError(testString1)))
           .catchError((e, st) => e);
 
       final res2 = Future(
               () => callbackCompleter.run(() => futureCauseError(testString2)))
           .catchError((e, st) => e);
 
-      expect(await res1, '$testStringError:$testString');
-      expect(await res2, '$testStringError:$testString');
+      expect(await res1, '$testStringError:$testString1');
+      expect(await res2, '$testStringError:$testString1');
     });
   });
+
+  group(
+    'A group of test of CallbackCompleter with different equality arg',
+    () {
+      const testArg1 = 'arg1';
+      const testArg2 = 'arg2';
+      const testArg3 = 'arg3';
+      const testArg4 = 'arg4';
+
+      test('Test CallbackCompleter runner. Unequal args', () async {
+        final callbackCompleter = CallbackCompleter<String>();
+
+        expect(
+            await callbackCompleter.run(
+                () async => futureTestString(testString1), testArg1),
+            testString1);
+
+        expect(
+            await callbackCompleter.run(
+                () => futureTestString(testString2), testArg2),
+            testString2);
+      });
+
+      test('Test CallbackCompleter multiple call. All args unequal', () async {
+        final callbackCompleter = CallbackCompleter<String>();
+
+        final res1 = Future(() => callbackCompleter.run(
+            () => futureTestString(testString1), testArg1));
+        final res2 = Future(() => callbackCompleter.run(
+            () => futureTestString(testString2), testArg2));
+        final res3 = Future(() => callbackCompleter.run(
+            () => futureTestString(testString3), testArg3));
+
+        expect(await res1, testString1);
+
+        final res4 = Future(() => callbackCompleter.run(
+            () => futureTestString(testString4), testArg4));
+
+        expect(await res2, testString2);
+        expect(await res3, testString3);
+
+        expect(await res4, testString4);
+      });
+
+      test(
+          'Test CallbackCompleter multiple call. Some args unequal. Queuing unequal args result',
+          () async {
+        final callbackCompleter = CallbackCompleter<String>();
+
+        final res1 = Future(() => callbackCompleter.run(
+            () => futureTestString(testString1), testArg1));
+        final res2 = Future(() => callbackCompleter.run(
+            () => futureTestString(testString2), testArg1));
+        final res3 = Future(() => callbackCompleter.run(
+            () => futureTestString(testString3), testArg2));
+
+        expect(await res1, testString1);
+
+        final res4 = Future(() => callbackCompleter.run(
+            () => futureTestString(testString4), testArg2));
+
+        expect(await res2, testString1);
+        expect(await res3, testString3);
+
+        expect(await res4, testString3);
+      });
+
+      test(
+          'Test CallbackCompleter multiple call. Some args unequal. Queuing equal args result',
+          () async {
+        final callbackCompleter = CallbackCompleter<String>();
+
+        final res1 = Future(() => callbackCompleter.run(
+            () => futureTestString(testString1), testArg1));
+        final res2 = Future(() => callbackCompleter.run(
+            () => futureTestString(testString2), testArg1));
+        final res3 = Future(() => callbackCompleter.run(
+            () => futureTestString(testString3), testArg2));
+
+        expect(await res3, testString3);
+
+        final res4 = Future(() => callbackCompleter.run(
+            () => futureTestString(testString4), testArg2));
+
+        expect(await res1, testString1);
+        expect(await res2, testString1);
+        expect(await res3, testString3);
+
+        expect(await res4, testString4);
+      });
+
+      test('Test CallbackCompleter exceptions handle. Unequal args', () async {
+        final callbackCompleter = CallbackCompleter<String>();
+
+        final res1 = Future(() => callbackCompleter.run(
+                () => futureTestString(testString1), testArg1))
+            .catchError((e, st) => '$e:$testString1');
+
+        final res2 = Future(() => callbackCompleter.run(
+                () => futureTestString(testString2), testArg2))
+            .catchError((e, st) => '$e:$testString2');
+
+        expect(await res1, testString1);
+        expect(await res2, testString2);
+      });
+
+      test(
+          'Test CallbackCompleter exceptions handle. Unequal args. Must throw and .catchError must replace exception',
+          () async {
+        final callbackCompleter = CallbackCompleter<String>();
+
+        final res1 = Future(() => callbackCompleter.run(
+                () => futureCauseError(testString1), testArg1))
+            .catchError((e, st) => e);
+
+        final res2 = Future(() => callbackCompleter.run(
+                () => futureCauseError(testString2), testArg2))
+            .catchError((e, st) => e);
+
+        expect(await res1, '$testStringError:$testString1');
+        expect(await res2, '$testStringError:$testString2');
+      });
+    },
+  );
 }
