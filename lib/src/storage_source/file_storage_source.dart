f3c9c15662cab716a133c7acf1d5b46e721e5c22
nvkantonio <nvkantonio@gmail.com>
2026-06-23 13:16:14 +0200

    add feat: FileStorageSource

---
+++ lib/src/storage_source/file_storage_source.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import 'package:meta/meta.dart';
+import 'package:storage_sources_core/storage_sources_core.dart';
+
+abstract interface class FileStorageSource<T>
+    implements ModifiableDataStorageSource<T> {
+  Future<int> writeFileAndUpdate(String filePath, List<int> bytes);
+
+  @protected
+  Future<SR<T>> fileResultFromPath(String path);
+
+  @protected
+  FutureOr<bool> doFileExist(T file);
+
+  @protected
+  FutureOr<String> getFilePath(T file);
+
+  @protected
+  FutureOr<void> deleteFile(T file);
+}
